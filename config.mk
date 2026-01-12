15a01ba01e5cf8a92ef7eae38b4f4efb493fb30b
The-projects <simplncprivate@gmail.com>
2026-01-12 22:42:33 +0000

    SYSTEM-CONFIG : Add prebuilt apps and build config for Bmobile
    SYSTEM-CONFIG : Add prebuilt apps and build config for Bmobile
    
    Added Android.bp and config.mk to include prebuilt DailyYou, ESpeak, NativeAlpha, and Recorder APKs in the build. This enables integration of these apps as presigned packages for Bmobile software.

---
+++ config.mk
@@ -0,0 +1,10 @@
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/bmobile/software
+
+# Prebuilt apps
+PRODUCT_PACKAGES += \
+    DailyYou \
+    ESpeak \
+    NativeAlpha \
+    Recorder
